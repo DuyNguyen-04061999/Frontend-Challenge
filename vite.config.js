19e0515e845eac84a689325f5b7e691dd650e6b2
Duy Nguyễn <nguyenduy30234@gmail.com>
2023-02-15 11:55:20 +0700

    setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+import { config } from "dotenv";
+config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT,
   },
   resolve: {
     alias: {
